5a7612561f8672009da9becba9b4f88370cf94eb
OmarElsehity <omarelsehity99@gmail.com>
2023-12-19 11:35:57 +0200

    pyfiglet

--- file.py
+++ file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-x = 10
-print ("Ana Hena ya Welad", x)
+# x = 10
+# print ("Ana Hena ya Welad", x)
 
 
+
+import pyfiglet
+result = pyfiglet.figlet_format("Omar Elsehity") 
+print (result)
 # da comment
